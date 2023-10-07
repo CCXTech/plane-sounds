94b4442c851834228554b762fb0310ebe01337fc
Christian Sinatsch <christian.sinatsch@delos.com>
2023-10-07 13:11:58 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 async function test() {
-    const { ETOPO1 } = await import('global-elevation');
-    let etopo1 = new ETOPO1();
-    let elevation = await etopo1.getElevation(-81.20819380065467, 29.4725396627256);
-    let elevation2 = await etopo1.getElevation(-81.54641234865, 39.4725396627256);
-    let elevation3 = await etopo1.getElevation(-71.20819380065467, 59.4725396627256);
-    let elevation4 = await etopo1.getElevation(-80.20819380065467, 19.4725396627256);
-    let elevation5 = await etopo1.getElevation(-82.20819380065467, 28.4725396627256);
+    const { ETOPO5 } = await import('global-elevation');
+    let etopo5 = new ETOPO5();
+    let elevation = await etopo5.getElevation(-81.20819380065467, 29.4725396627256);
+    let elevation2 = await etopo5.getElevation(-81.54641234865, 39.4725396627256);
+    let elevation3 = await etopo5.getElevation(-71.20819380065467, 59.4725396627256);
+    let elevation4 = await etopo5.getElevation(-80.20819380065467, 19.4725396627256);
+    let elevation5 = await etopo5.getElevation(-82.20819380065467, 28.4725396627256);
     console.log(elevation);
     console.log(elevation2);
     console.log(elevation3);
