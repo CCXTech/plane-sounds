e044488fa7e95061d4242ee0a411eb70075193fa
Christian Sinatsch <christian.sinatsch@delos.com>
2023-10-09 15:45:46 -0400

    update

--- index.js
+++ index.js
@@ -3,20 +3,9 @@ const senseHatAPI = require('./lib/senseHatAPI');
 const emmiter = require('events');
 
 const altitude = new altitudeCalculator();
-const senseHat = new senseHatAPI();
 
 
 async function start() {
-    altitude.on('AGLAltitude', (AGLAltitude) => {
-        console.log('Got AGL Alt')
-        console.log(AGLAltitude);
-    })
-    senseHat.on('verticalSpeed', (verticalSpeed) => {
-        this.verticalSpeed = verticalSpeed;
-        console.log('Got Vertical Speed' + verticalSpeed)
-    })
-    await altitude.init();
-    await senseHat.init();
 }
 
 start();
