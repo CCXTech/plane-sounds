b473a399dc185e4da79ee40d4b3a6f4c1b55d0a8
Christian Sinatsch <christian.sinatsch@delos.com>
2023-10-09 16:21:23 -0400

    updates

--- index.js
+++ index.js
@@ -1,51 +1,28 @@
-const altitudeCalculator = require('./lib/altitudeCalculator');
-const announciator = require('./lib/annunciator');
-const emmiter = require('events');
 const exec = require('child_process').exec;
 const path = require('path');
 const filePrefix = path.join(__dirname, 'sounds/');
-
-const altitude = new altitudeCalculator();
-let sattelites = 0;
-
-let ann = null;
-
+const annunciator = require('./lib/annunciator.js');
 
 async function start() {
     await playInitialSound();
+    await sleep(5000);
+
+    const ann = new annunciator();
+    await ann.init();
 
 
-
-    altitude.on('numSats' , (numSats) => {
-        sattelites = numSats;
-        isAvailable();
-    })
-
-    await altitude.init();
-
-}
-
-async function isAvailable() {
-    if (sattelites > 5 && ann == null) {
-        await playAvailableSound();
-        ann = new announciator();
-        await ann.init();
-    } else if (sattelites < 5 && ann != null) {
-        await playOfflineSound();
-        ann = null;
-    }
 }
 
 async function playInitialSound() {
     await exec('ffplay -autoexit ' + filePrefix + 'init.mp3')
 }
 
-async function playOfflineSound() {
-    await exec('ffplay -autoexit ' + filePrefix + 'offline.mp3')
-}
-
-async function playAvailableSound() {
-    await exec('ffplay -autoexit ' + filePrefix + 'Available.mp3')
+async function sleep(ms) {
+    return new Promise((res) => {
+        setTimeout(() => {
+            res();
+        }, ms)
+    })
 }
 
 start();
